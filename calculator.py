d69b1d6a47841f287452fed289d3a00ca670fe69
hillel aharoni <hillel2552022@gmail.com>
2026-02-03 10:22:32 +0200

    added divide func

--- calculator.py
+++ calculator.py
@@ -7,3 +7,8 @@ def sub(a , b):
 def multiply(a,b):
     """multiply two numbers"""
     return a*b
+
+def safe_divide(a, b):
+    if b == 0:
+        return "Error: cant be divide by 0 "
+    return a / b
